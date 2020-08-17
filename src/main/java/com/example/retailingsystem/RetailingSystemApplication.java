fe91b5eeb2e410af8aee4c9a903498bdaf9fccd2
Franz Nico Testado <2195568@slu.edu.ph>
2020-08-17 11:43:50 +0800

    initial commit

---
+++ src/main/java/com/example/retailingsystem/RetailingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.retailingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailingSystemApplication.class, args);
+	}
+
+}

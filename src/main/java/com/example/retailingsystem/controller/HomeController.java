f9505aeb653a1c4c4a1941eb0112d7f3bd7e2ad2
Franz Nico Testado <2195568@slu.edu.ph>
2020-08-18 16:12:36 +0800

    updated home.html & HomeController.java

--- src/main/java/com/example/retailingsystem/controller/HomeController.java
+++ src/main/java/com/example/retailingsystem/controller/HomeController.java
@@ -1,12 +1,24 @@
 package com.example.retailingsystem.controller;
 
+import com.example.retailingsystem.model.Sale;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String home() {
+    @GetMapping
+    public String showSales(Model model) {
+        final List<Sale> sales = Arrays.asList(
+                new Sale("1", 30.00, "..."),
+                new Sale("2", 20.00, "..."),
+                new Sale("3", 15.00, "...")
+        );
+
+        model.addAttribute("sales", sales);
         return "home";
     }
 }
